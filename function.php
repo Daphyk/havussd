8c8d21d4ad17d7bae7846445065b8cd36a2594db
kendid <81610710+Daphyk@users.noreply.github.com>
2022-02-26 23:51:58 +0300

    Update function.php

--- function.php
+++ function.php
@@ -18,4 +18,49 @@ function newname ()
 {$response = "CON Please enter new name";
     return $response;
     */
+// error
+/*
+function affliates()
+{
+    $response = "CON";
+    $response .= "1. Register as an Affliate \n";
+    $response .= "2. Invite your friends\n";
+  	return $response;
+}
+function myNumber($phoneNumber)
+{
+    $response = "END Your phone number is $phoneNumber";
+    return $response;
+}
+
+function editName()
+{
+    $response = "CON Please enter new name";
+    return $response;
+}
+function faqs()
+{
+    $response = "END Follow us on our social media platforms or contact us on 0723880313\n";
+    return $response;
+}
+function optout()
+
+{
+    $response="CON Are you sure you want to Opt Out \n";
+    $response.= "1. Yes \n";
+    $response.= "2.No \n";
+    return $response;
+}
+function optoutSuccess()
+
+{
+    $response="END You have successfully opted out";
+    return $response;
+}
+function AlternativeNumber()
+
+{
+    $response = "CON Please enter your alternative number \n";
+    return $response;
+}
 ?>
