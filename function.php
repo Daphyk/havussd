6b060104eed6961ff1ccc3f6bf30e416c05854cf
kendid <81610710+Daphyk@users.noreply.github.com>
2022-02-28 23:58:13 +0300

    Update function.php

--- function.php
+++ function.php
@@ -21,6 +21,26 @@ function newname ()
 // error
 /*
 /*
+
+function whatsNew()
+{
+            $response ="CON Trainees center \n";
+            $response .= "welcome to trainees center to continue in enlish press 1 to continue in kiswahili press 2 \n";
+            $response .= "00.Main ";
+            $response .= "0.Back";
+  			return $response;
+}
+function products()
+{
+    $response = "CON";
+    $response .= "1. Top up Airtime \n";
+    $response .= "2. Add voice Option \n";
+    $response .= "00.Main Menu";
+    $response .= "0.Back";
+    $response .= "00.Main Menu";
+    $response .= "0.Back";
+  	return $response;
+}
 function affliates()
 {
     $response = "CON";
